d23b6b1bd6a543ae67a58182d8df021a9db794c9
Ahmed Hany <ah7608867@gmail.com>
2024-11-30 15:01:25 +0200

    Apache Benchmark and Cluster performance VS CPU Cores

--- Node Performance/index.js
+++ Node Performance/index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const cluster = require('cluster'); // to use the cluster module
+const crypto = require('crypto')
+process.env.UV_THREADPOOL_SIZE = 1; // to change the thread pool size
 
 
 if (cluster.isMaster) {
     // if we are in the master thread, we will create a new child thread
-    cluster.fork(); // here is the first child thread
-    cluster.fork(); // here is the second child thread
-    cluster.fork(); // here is the third child thread
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    // for more performance, we need to create a child thread for each CPU in the machine VERY IMPORTANT
     // we can create as many child threads as we want
     // we will create a child thread for each CPU in the machine
     // this will make the server faster and more efficient
@@ -28,13 +34,21 @@ else {
     */
 
     app.get('/', (req, res) => {
-        doWork(5000); // this will block the event loop for 5 seconds
-        res.status(200).send('Hi there');
+        //doWork(5000); // this will block the event loop for 5 seconds
+        crypto.pbkdf2('password', 'salt', 100000, 512, 'sha512', () => {
+            res.status(200).send('Hi there');
+        });
+
     });
 
     // although this route is fast, it will not be served until the first request is served
     app.get('/fast', (req, res) => {
         res.status(200).send('This is fast');
+    });
+
+    app.get('/fast2', (req, res) => {
+        doWork(200);
+        res.status(200).send('This is fast2');
     });
 
     app.listen(3000, () => {
